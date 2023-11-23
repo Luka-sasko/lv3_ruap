a63801030c8d6c56ecc57c6e264bc31dea336dac
Luka-sasko <74216417+Luka-sasko@users.noreply.github.com>
2023-11-23 19:12:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -64,6 +64,28 @@ $host = "lvpatrikluka-server.mysql.database.azure.com";
 $user = "mnctbuband";
 $pwd = "2RC268L6846CF670$";
 $db = "lvpatrikluka-database";
+
+$conn = mysqli_connect($host,$user,$pwd);
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else {
+echo 'Connected successfully';
+}
+
+mysqli_select_db($conn,$db);
+$sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+VARCHAR(30), date DATE);";
+if ($conn->query($sql_create) === TRUE) {
+echo "Table registration_tbl created successfully";
+} else {
+echo "Error creating table: " . $conn->error;
+}
+$conn->close();
+
 // Connect to database.
 $conn = mysqli_connect($host, $user, $pwd, $db);
 if (mysqli_connect_errno())
